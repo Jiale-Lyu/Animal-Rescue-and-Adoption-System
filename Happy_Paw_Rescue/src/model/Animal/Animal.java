a2e023edb48bc2ed86d136ade707b5e932e7eac6
Jiale Lyu <lyu.jial@northeastern.edu>
2022-12-04 21:38:58 -0500

    update Animal

--- Happy_Paw_Rescue/src/model/Animal/Animal.java
+++ Happy_Paw_Rescue/src/model/Animal/Animal.java
@@ -8,8 +8,155 @@ package model.Animal;
  *
  * @author Jiale Lyu
  */
+
+import javax.swing.ImageIcon;
+import model.Enterprise.AnimalShelterEnterprise;
+import model.UserAccount.UserAccount; 
+import model.WorkQueue.WorkQueue;
+
+
 public class Animal {
+      
+    private AnimalShelterEnterprise enterprise;
     private int id;
     private String name;
-    private String gender;
+    private String sex;
+    private String weight;
+    private String photo;
+    private String area;
+    private WorkQueue workQueue;
+    private ImageIcon photoIcon;
+    private String vetMessage;
+    private String baviorCheckMessage;
+    private String pharmacyMessage;
+    private UserAccount manager;
+    private UserAccount adoptor;
+    private boolean adpotionRequested;
+    
+    //Animal constructor
+    public Animal(String nm) {
+        name = nm;
+        this.workQueue = new WorkQueue();
+    }
+    
+    //getter and setter
+    public void setEnterprise(AnimalShelterEnterprise enterprise) {
+        this.enterprise = enterprise;
+    }
+
+    public AnimalShelterEnterprise getEnterprise() {
+        return enterprise;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getArea() {
+        return area;
+    }
+
+    public void setArea(String area) {
+        this.area = area;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public String getWeight() {
+        return weight;
+    }
+
+    public void setWeight(String weight) {
+        this.weight = weight;
+    }
+    public void setPhotoIcon(ImageIcon photoIcon) {
+        this.photoIcon = photoIcon;
+    }
+
+    public String getVetMessage() {
+        return vetMessage;
+    }
+    public String getPhoto() {
+        return photo;
+    }
+
+    public void setPhoto(String photo) {
+        this.photo = photo;
+    }
+
+    public ImageIcon getPhotoIcon() {
+        return photoIcon;
+    }
+
+    public void setVetMessage(String vetMessage) {
+        this.vetMessage = vetMessage;
+    }
+
+    public String getPharmacyMessage() {
+        return pharmacyMessage;
+    }
+
+    public void setPharmacyMessage(String pharmacyMessage) {
+        this.pharmacyMessage = pharmacyMessage;
+    }
+
+    public String getBaviorCheckMessage() {
+        return baviorCheckMessage;
+    }
+
+    public void setBaviorCheckMessage(String baviorCheckMessage) {
+        this.baviorCheckMessage = baviorCheckMessage;
+    }
+
+    public UserAccount getManager() {
+        return manager;
+    }
+
+    public void setManager(UserAccount manager) {
+        this.manager = manager;
+    }
+
+    public UserAccount getAdoptor() {
+        return adoptor;
+    }
+
+    public void setAdpotionRequested(boolean adpotionRequested) {
+        this.adpotionRequested = adpotionRequested;
+    }
+
+    public void setAdoptor(UserAccount adoptor) {
+        this.adoptor = adoptor;
+    }
+
+    public boolean isAdpotionRequested() {
+        return adpotionRequested;
+    }
+    
+    public WorkQueue getWorkQueue() {
+        return workQueue;
+    }
+    
+    @Override
+    public String toString(){
+        return name;
+    }
+    
 }
--- Happy_Paw_Rescue/src/model/Animal/AnimalDirectory.java
+++ Happy_Paw_Rescue/src/model/Animal/AnimalDirectory.java
@@ -2,38 +2,43 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
+
 package model.Animal;
-
-import java.util.ArrayList;
-
 /**
  *
  * @author Jiale Lyu
  */
+
+import java.util.ArrayList;
+import model.Enterprise.AnimalShelterEnterprise;
+import model.UserAccount.UserAccount;
+import model.Enterprise.Enterprise;
+
+
 public class AnimalDirectory {
-    private ArrayList<Animal> animalList;
     
-    public AnimalDirectory() {
+    private final ArrayList<Animal> animalList;
+    private final Enterprise enterprise;
+
+    public AnimalDirectory(Enterprise enterprise) {
         this.animalList = new ArrayList<Animal>();
-    
+        this.enterprise = enterprise;
     }
-    
+
     public ArrayList<Animal> getAnimalList() {
         return animalList;
     }
-        
+
+    public Enterprise getEnterprise() {
+        return enterprise;
+    }
+    
     public Animal createAnimal(String name){
         Animal animal = new Animal(name);
-//        animal.setEnterprise((AnimalShelterEnterprise) enterprise);
+        animal.setEnterprise((AnimalShelterEnterprise) enterprise);
         animal.setId(animalList.size()+1);
         animalList.add(animal); 
         return animal;
-    }
-    public Animal getAnimalByName(String name){
-        for (Animal a : animalList){
-            if (name.equalsIgnoreCase(a.getName())) return a;
-        }
-        return null;
     }
     
     public boolean nameIsUnique(String name){
@@ -44,12 +49,25 @@ public class AnimalDirectory {
         }
         return true;
     }
-   
-    public Animal getAnimalByAdoptionRequested(Boolean b){
-        for(Animal a:animalList){
-            if(b==true) return a;
+    
+    public Animal getAnimalByName(String name){
+        for (Animal aa : animalList){
+            if (name.equalsIgnoreCase(aa.getName())) return aa;
         }
         return null;
     }
     
+    public Animal getAnimalByManager(UserAccount manager){
+        for (Animal aa : animalList) {
+            if (aa.getManager() == manager && aa.getAdoptor() == null) return aa;
+        }
+        return null;
+    }
+    
+    public Animal getAnimalByAdoptionRequested(Boolean bb){
+        for(Animal aa:animalList){
+            if(bb==true) return aa;
+        }
+        return null;
+    }
 }
