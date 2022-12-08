3962f0c4694a372527639d589b45e7b6cc87a24f
fanyesun <sun.fany@northeastern.edu>
2022-12-08 10:11:33 -0500

    Create AdopterAuthorizationRequest.java
    Workqueue

---
+++ src/model/WorkQueue/AdopterAuthorizationRequest.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model.WorkQueue;
+
+/**
+ *
+ * @author fanyesun
+ */
+public class AdopterAuthorizationRequest extends WorkRequest {
+    
+    
+}
