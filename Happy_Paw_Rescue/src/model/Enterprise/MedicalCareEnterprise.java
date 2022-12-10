c5f5a26659a93fbb267ce49f9ae47b641fc5fe14
Yifei Chen <chen.yifei5@northeastern.edu>
2022-12-10 13:08:42 -0500

    MedicalCareEnterprise

---
+++ Happy_Paw_Rescue/src/model/Enterprise/MedicalCareEnterprise.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model.Enterprise;
+
+
+/**
+ *
+ * @author Yifei Chen
+ */
+public class MedicalCareEnterprise extends Enterprise {
+    
+    
+    public MedicalCareEnterprise(String name) {
+        
+        super(name, Enterprise.Type.MedicalCare);
+    }    
+    
+}
