4da56f3212f8ec7da44fe20f92db6d6df9e7175f
Jiale Lyu <lyu.jial@northeastern.edu>
2022-12-10 13:32:20 -0500

    add work request

---
+++ Happy_Paw_Rescue/src/model/WorkQueue/WorkRequest.java
@@ -0,0 +1,102 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model.WorkQueue;
+
+import model.UserAccount.UserAccount;
+import java.util.Date;
+import model.Animal.Animal;
+
+/**
+ *
+ * @author Jiale Lyu
+ */
+public abstract class WorkRequest {
+
+    private String message;
+    private UserAccount sender;
+    private UserAccount receiver;
+    private String status;
+    private Date requestDate;
+    private Date resolveDate;
+    private Animal animal;
+    private String userEmailId;
+
+    public WorkRequest() {
+        requestDate = new Date();
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public UserAccount getSender() {
+        return sender;
+    }
+
+    public void setSender(UserAccount sender) {
+        this.sender = sender;
+    }
+
+    public UserAccount getReceiver() {
+        return receiver;
+    }
+
+    public void setReceiver(UserAccount receiver) {
+        this.receiver = receiver;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public Date getRequestDate() {
+        return requestDate;
+    }
+
+    public void setRequestDate(Date requestDate) {
+        this.requestDate = requestDate;
+    }
+
+    public Date getResolveDate() {
+        return resolveDate;
+    }
+
+    public void setResolveDate(Date resolveDate) {
+        this.resolveDate = resolveDate;
+    }
+
+    public Animal getAnimal() {
+        return animal;
+    }
+
+    public void setAnimal(Animal animal) {
+        this.animal = animal;
+    }
+
+    
+    
+    public String getUserEmailId() {
+        return userEmailId;
+    }
+
+    public void setUserEmailId(String userEmailId) {
+        this.userEmailId = userEmailId;
+    }
+
+    
+    @Override
+    public String toString() {
+        return this.message;
+    }
+
+}
